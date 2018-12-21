ad4cf7df384634091f3d94cb4c3d3730c0d10498
Josh Thompson <jthomp43@uoguelph.ca>
2018-12-21 10:15:25 -0500

    Initial implementation of parameterized rules. Needs heavy refactoring.

--- examples/param_system.cpp
+++ examples/param_system.cpp
@@ -14,16 +14,23 @@ int main(int argc, char** argv) {
   LSymbolType B('B', parameterSet({LINT}));
 
   LSymbol A_(A); //define symbols needed for axiom and rules, as well as their parameters
-  A_.setFloatParam(0.5f, 0);
-  A_.setFloatParam(-0.5f, 1);
-  A_.setFloatParam(0.25f, 2);
+  A_.setFloatParam(500.005f, 0);
+  A_.setFloatParam(200.002f, 1);
+  A_.setFloatParam(-100.001f, 2);
   LSymbol B_(B);
   B_.setIntParam(50285, 0);
 
-  LRule A_AB(A, {A, B}); //define rules
+  LRule A_AB(A, {A, B}, {
+    {
+      LRuleParameterReference(0, 1, LFLOAT),
+      LRuleParameterReference(1, 2, LFLOAT),
+      LRuleParameterReference(2, 0, LFLOAT),
+    }
+  }); //define rules
+
   LRule B_A(B, {A});
 
-  LSystem system({LSymbol(A)}); //define the system using an axiom
+  LSystem system({A_}); //define the system using an axiom
 
   system.setRule(A, A_AB); //add the rules to the system
   system.setRule(B, B_A);
--- include/l_system/l_rule.h
+++ include/l_system/l_rule.h
@@ -9,31 +9,93 @@ namespace l_system::rule {
   using namespace symbol;
   using namespace param;
 
+  class LRuleParameterReference {
+
+    LParameterCount sourceIndex_;
+    LParameterCount destIndex_;
+    LParameter type_;
+    //ParameterTransformation transformation_;
+
+  public:
+    LRuleParameterReference(LParameterCount sourceIndex, LParameterCount destIndex, LParameter type) : sourceIndex_(sourceIndex), destIndex_(destIndex), type_(type) {}
+
+    auto sourceIndex() const noexcept -> LParameterCount {
+
+      return sourceIndex_;
+    }
+
+    auto destIndex() const noexcept -> LParameterCount {
+
+      return destIndex_;
+    }
+
+    auto type() const noexcept -> LParameter {
+
+      return type_;
+    }
+  };
+
+  using LRuleParamRefSet = std::vector<std::vector<LRuleParameterReference>>;
+
   template <typename T>
   class LRule {
 
     LSymbolType<T> predecessor_;
     LTypeString<T> result_;
+    LRuleParamRefSet paramTransforms_;
 
   public:
 
-    LRule(LSymbolType<T> predecessor, LTypeString<T> result) : predecessor_(predecessor), result_(result) {}
+    LRule(LSymbolType<T> predecessor, LTypeString<T> result, LRuleParamRefSet paramTransforms = {}) :
+      predecessor_(predecessor),
+      result_(result),
+      paramTransforms_(paramTransforms) {}
 
-    LRule(LSymbolType<T> predecessor, std::initializer_list<LSymbolType<T>> result) : predecessor_(predecessor), result_(result) {}
+
 
     auto result() const noexcept -> LTypeString<T> {
 
       return result_;
     }
 
-    auto operator()([[maybe_unused]] LSymbol<T> inputSymbol, [[maybe_unused]] LString<T> before, [[maybe_unused]] LString<T> after) const noexcept -> LString<T> {
+    //This operator needs to be refactored
+    auto operator()(LSymbol<T> inputSymbol, [[maybe_unused]] LString<T> before, [[maybe_unused]] LString<T> after) const noexcept -> LString<T> {
 
       LString<T> result;
       result.reserve(result_.size());
 
-      for(auto l : result_) {
+      //for if for switch????? why
+      for(size_t symbolIndex = 0; symbolIndex < result_.size(); ++symbolIndex) {
 
-        result.emplace_back(LSymbol<T>(l));
+        auto resSymbol = LSymbol<T>(result_.at(symbolIndex));
+
+        if(symbolIndex < paramTransforms_.size()) {
+
+          for(size_t paramIndex = 0; paramIndex < paramTransforms_.at(symbolIndex).size(); ++paramIndex) {
+
+            auto reference = paramTransforms_.at(symbolIndex).at(paramIndex);
+
+            switch(reference.type()) { //TODO get rid of this switch statement
+
+              case LCHAR:
+                resSymbol.setCharParam(inputSymbol.getCharParam(reference.sourceIndex()), reference.destIndex());
+                break;
+              case LINT:
+                resSymbol.setIntParam(inputSymbol.getIntParam(reference.sourceIndex()), reference.destIndex());
+                break;
+              case LFLOAT:
+                resSymbol.setFloatParam(inputSymbol.getFloatParam(reference.sourceIndex()), reference.destIndex());
+                break;
+              case LCUSTOM:
+                resSymbol.setCustomParam(inputSymbol.getCustomParam(reference.sourceIndex()), reference.destIndex());
+                break;
+              default:
+                break;
+            }
+          }
+        }
+
+        result.emplace_back(resSymbol);
       }
 
       return result;
